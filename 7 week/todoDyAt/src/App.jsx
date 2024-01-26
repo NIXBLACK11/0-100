b3e1eef9207924278cf86fb98e545b36e5429967
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-01-26 14:53:26 +0530

    week 7

--- 7 week/todoDyAt/src/App.jsx
+++ 7 week/todoDyAt/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilState, useRecoilStateLoadable, useRecoilValue } from "recoil";
 import { todosAtomFamily } from './atoms';
 
 function App() {
@@ -7,17 +7,34 @@ function App() {
     <RecoilRoot>
       <Todo id={1}/>
       <Todo id={2}/>
+      <Todo id={1}/>
+      <Todo id={2}/>
+      <Todo id={1}/>
+      <Todo id={2}/>
+      <Todo id={2}/>
     </RecoilRoot>
   )
 }
 
 function Todo({id}) {
-  const currentTodo = useRecoilValue(todosAtomFamily(id));
-  return <div>
-    {currentTodo.title}
-    <br/>
-    {currentTodo.description}
-  </div>
+  // const currentTodo = useRecoilValue(todosAtomFamily(id));
+  const [todo, setTodo] = useRecoilStateLoadable(todosAtomFamily(id));
+  if(todo.state === "loading") {
+    return <div>
+      loading ...
+    </div>
+  }
+  else if(todo.state === "hasValue") {
+    return <div>
+        {todo.contents.title}
+        <br/>
+        {todo.contents.description}
+    </div>
+  } else {
+    return <div>
+      Error while getting data
+    </div>
+  }
 }
 
 export default App
--- 7 week/todoDyAt/src/atoms.js
+++ 7 week/todoDyAt/src/atoms.js
@@ -1,9 +1,17 @@
-import { atomFamily } from "recoil";
-import { TODOS } from "./todos.js";
+import { atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
+// import { TODOS } from "./todos.js";
 
 export const todosAtomFamily = atomFamily({
     key: "todosAtomFamily",
-    default: id => {
-        return TODOS.find(x => x.id===id)
-    }
+    // default: id => {
+    //     return TODOS.find(x => x.id===id)
+    // }
+    default: selectorFamily({
+        ket: "todoSelectorFamily",
+        get: (id) => async ({get}) => {
+            const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+            return res.data.todo;
+        }
+    })
 });
