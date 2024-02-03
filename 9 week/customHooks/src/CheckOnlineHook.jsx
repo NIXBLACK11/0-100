972cbb2408a22a6af22195ff525238b9fdec9d7b
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-02-03 19:15:20 +0530

    hooks

--- 9 week/customHooks/src/CheckOnlineHook.jsx
+++ 9 week/customHooks/src/CheckOnlineHook.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function userIsOnline() {
-    const [onLine, setOnLine] = useState(false);
-    window.addEventListener('online', () => {
-        console.log("User Online");
-        setOnLine(true);
-    });
-    window.addEventListener('offline', () => {
-        console.log("User OffLine");
-        setOnLine(false);
-    });
+    const [onLine, setOnLine] = useState(window.navigator.onLine);
+    useEffect(() => {
+        window.addEventListener('online', () => {
+            console.log("User Online");
+            setOnLine(true);
+        });
+        window.addEventListener('offline', () => {
+            console.log("User OffLine");
+            setOnLine(false);
+        });
+    }, []);
     return onLine;
 }
 
