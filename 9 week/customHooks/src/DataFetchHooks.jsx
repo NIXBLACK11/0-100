5a81c622314b5bbee87076ca11351914900b8f5c
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-02-03 18:57:38 +0530

    hooks

--- 9 week/customHooks/src/DataFetchHooks.jsx
+++ 9 week/customHooks/src/DataFetchHooks.jsx
@@ -1,32 +1,41 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function useTodos() {
+function useTodos(n) {
     const[todos, setTodos] = useState([]);
     const[loading, setLoading] = useState(true);
 
     useEffect(async () => {
+        const clockValue = setInterval(async () => {
+            const resp = await axios.get("https://sum-server.100xdevs.com/todos");
+            setTodos(resp.data.todos);
+            setLoading(false);
+        }, n*1000);
         const resp = await axios.get("https://sum-server.100xdevs.com/todos");
-        setTodos(resp.data.todos);
-        setLoading(false);
-    }, []);
+            setTodos(resp.data.todos);
+            setLoading(false);
+        //cleanup the clock if n changes
+        return () => {
+            clearInterval(clockValue);
+        }
+    }, [n]);
 
     return { todos, loading };
 }
 
 
 function DataFetchHooks() {
-    const { todos, loading } = useTodos();
+    const { todos, loading } = useTodos(5);
     if(loading) {
         return <div>
             Loading
         </div>
     }
 
-    if (!Array.isArray(todos)) {
+    if (!Array.isArray(todos)) { 
         return <div>No todos available</div>;
     }
-    
+
     return <div>
         {todos.map(todo => <Track todo={ todo }/>)}
     </div>
