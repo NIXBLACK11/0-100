766f6bcc8349e65524b1d34921633369d0add3ca
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-04-25 17:12:29 +0530

    started next

---
+++ 14 week/next-app/app/signup/page.tsx
@@ -0,0 +1,9 @@
+// The name of function does not matter and can be omitted
+
+export default function Signup() {
+  return (
+    <div>
+      signup
+    </div>
+  );
+}
---
+++ 14 week/readme.md
@@ -0,0 +1,8 @@
+# NEXT.JS
+
+![Alt text](image1.png)
+
+![Alt text](image.png)
+
+getting started 
+npx create-next-app@latest
