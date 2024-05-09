491f03602c6429b54bbf20325ebd08f272b25433
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-05-09 20:05:06 +0530

    native web socket

---
+++ 19 week/ws/src/Nativeindex.ts
@@ -0,0 +1,25 @@
+import WebSocket, { WebSocketServer } from "ws";
+import http from 'http';
+
+const server = http.createServer(function(req: any, res: any) {
+    console.log((new Date()) + `Received request for ${req.url}`);
+    res.end("hi there");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            client.send(data, { binary: isBinary });
+        });
+    });
+
+    ws.send('Hello! Message from server!!');
+});
+
+server.listen(8080, function () {
+    console.log(new Date() + 'Server is listening on port 8080');
+});
